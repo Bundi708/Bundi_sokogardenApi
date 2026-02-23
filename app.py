12f53eb2d816c387dbb40691af26dd13854311ae
Bundi708 <boscobundikirema10401@gmail.com>
2026-02-23 12:34:17 +0300

    added images

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import *
 import pymysql
+import os
 
 
 app = Flask (__name__)
+app.config["UPLOAD_FOLDER"]= "static/images"
 
 @app.route("/api/signup",methods = ["POST"])
 def signup():
@@ -59,6 +61,45 @@ def signin():
         # get user data
         user= cursor.fetchone()
         return jsonify({"message":"log in successful","user":user}) 
+    
+
+@app.route("/api/add_product", methods=["POST"])
+def addproduct():
+    product_name = request.form["product_name"]
+    product_description = request.form["product_description"]
+    product_category = request.form["product_category"]
+    product_cost = request.form["product_cost"]
+    product_image = request.files["product_image"]
+
+    
+
+    print(product_name,product_description,product_category,product_cost,product_image)
+
+    # get image name
+    image_name = product_image.filename
+    print(image_name)
+
+    # ssave image to the  images folder
+    file_path= os.path.join(app.config["UPLOAD_FOLDER"], image_name)
+
+    product_image.save(file_path)
+
+    # create a connection to the database
+    connection= pymysql.connect(host="localhost", user="root", password="", database="bundi_sokogarden")
+
+    # create a cursor
+    cursor = connection.cursor()
+    # sql
+    sql= "insert into product_details(product_name, product_description, product_category, product_cost, product_image) values(%s, %s, %s, %s, %s)"
+    # data to execute in sql querry
+    data= (product_name, product_description, product_category, product_cost,image_name)
+    # execute query
+    cursor.execute(sql, data)
+    # save the data
+    connection.commit()
+
+    return jsonify({"message":"product added successfully"})
+
 
 
 if __name__ == "__main__":
